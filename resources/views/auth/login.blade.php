306687c6306fa43e2dac37021cc95ce1333fdaa5
Tạ Tùng <tung9ah@gmail.com>
2018-04-19 18:26:30 +0700

    Thêm Title

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
-@extends('layouts.app')
-
+@extends('auth.layout')
+@section('title','Login | Blog Train')
 @section('content')
-<div class="container">
+{{-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -65,5 +65,38 @@
             </div>
         </div>
     </div>
-</div>
+</div> --}}
+    <div class="w3layoutscontaineragileits">
+    <h2>Login</h2>
+        <form method="POST" action="{{ route('login') }}">
+                        @csrf
+            <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocu placeholder="Nhập email" s>
+            <br>
+            @if ($errors->has('email'))
+                <span class="invalid-feedback">
+                    <strong>{{ $errors->first('email') }}</strong>
+                </span>
+             @endif
+            <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required placeholder="Nhập password">
+            <br>
+            @if ($errors->has('password'))
+                <span class="invalid-feedback">
+                    <strong>{{ $errors->first('password') }}</strong>
+                </span>
+            @endif
+            <ul class="agileinfotickwthree">
+                <li>
+                    <input type="checkbox" id="brand1" value="">
+                    <label for="brand1"><span></span>Remember me</label>
+                    <a href="#">Forgot password?</a>
+                </li>
+            </ul>
+            <div class="aitssendbuttonw3ls">
+                <input type="submit" value="LOGIN">
+                <p> To register new account <span>→</span> <a class="w3_play_icon1" href="#small-dialog1"> Click Here</a></p>
+                <div class="clear"></div>
+            </div>
+        </form>
+    </div>
+
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/admin','UserController@index');
+Route::get('/title','TitleController@index');
+Route::post('/title/add','TitleController@create')->name('addTitle');
+
